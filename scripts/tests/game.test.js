980da3934bc744005ca4912c4736babe1e5684b0
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-12-08 20:59:25 +0000

    Adding gamer tests

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { game } = require("../game");
+
+beforeAll(() => {
+    // install fs libary 
+    let fs = require("fs");
+    // read file index.html with utf character set
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    // open document
+    document.open();
+    // write the file contents to it
+    document.write(fileContents);
+    // document close
+    document.close();
+});
+
+describe("game objects contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+});
+
